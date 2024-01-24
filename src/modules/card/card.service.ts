597137362b527a7e6cf934d81f06d809f38c0a85
LutinJr <72550004+PierreGourgouillon@users.noreply.github.com>
2024-01-24 20:26:32 +0100

    feature/crud-cards (#4)

--- src/modules/card/card.service.ts
+++ src/modules/card/card.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CardDTO } from 'src/dto/card.dto';
+import { Card } from 'src/mongo/models/card.model';
+import { DataType } from 'src/mongo/repositories/base.repository';
 import { CardRepository } from 'src/mongo/repositories/card.repository';
 
 @Injectable()
@@ -6,4 +9,70 @@ export class CardService {
   constructor(
     private readonly cardRepository: CardRepository
   ) {}
+
+  async createOne(cardData: CardDTO): Promise<Card> {
+    try {
+      const response = await this.cardRepository.insert(
+        {
+          name: cardData.name,
+          dishes: cardData.dishes,
+          isActive: cardData.isActive,
+          creationDate: cardData.creationDate
+        }
+      )
+
+      return response as Card
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException();
+    }
+  }
+
+  async findAll(): Promise<Card[]> {
+    try {
+      const response = await this.cardRepository.findAll()
+
+      return response as Card[]
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException();
+    }
+  }
+
+  async findOne(id: string): Promise<Card> {
+    try {
+      const response = await this.cardRepository.findOneBy({ _id: id })
+
+      return response as Card
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException();
+    }
+  }
+
+  async updateOne(id: string, cardData: DataType): Promise<Card> {
+    try {
+      const isUpdate = await this.cardRepository.updateOneBy({ _id: id }, cardData)
+
+      if (!isUpdate) {
+        throw new BadRequestException();
+      }
+
+      const response = await this.findOne(id)
+
+      return response as Card
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException();
+    }
+  }
+
+  async deleteOne(id: string) {
+    try {
+      await this.cardRepository.deleteOnyBy({ _id: id })
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException();
+    }
+  }
 }
