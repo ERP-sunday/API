a4202d31ad2aa8bafe32d5d6dc1f08fcf80ec7ca
Pierre <Pierre_gourgouillon@hotmail.com>
2024-01-24 20:11:14 +0100

    finish

--- src/modules/dish/dish.controller.ts
+++ src/modules/dish/dish.controller.ts
@@ -1,15 +1,44 @@
-import {
-    Controller,
-    Get,
-  } from '@nestjs/common';
+import { Controller, Get, Body, Param, Put, Delete, Post } from '@nestjs/common';
 import { DishService } from './dish.service';
-  
+import { Response } from 'src/utils/response';
+import { Dish } from 'src/mongo/models/dish.model';
+import { DataType } from 'src/mongo/repositories/base.repository';
+import { DishDTO } from 'src/dto/dish.dto';
+
   @Controller('dishes')
   export class DishController {
     constructor(private readonly dishService: DishService) {}
   
+    @Post()
+    async createOne(@Body() dishData: DishDTO): Promise<Response<Dish>> {
+      const response = await this.dishService.createOne(dishData)
+
+      return { error: "", data: response }
+    }
+  
     @Get()
-    findAll(): string {
-        return "dishes"
+    async findAll(): Promise<Response<Dish[]>> {
+        const response = await this.dishService.findAll()
+
+        return { error: "", data: response }
+    }
+
+    @Get(":id")
+    async finOne(@Param() params: any): Promise<Response<Dish>> {
+      const response = await this.dishService.findOne(params.id)
+
+      return { error: "", data: response }
+    }
+
+    @Put(":id")
+    async updateOne(@Param() params: any, @Body() updateData: DataType): Promise<Response<Dish>> {
+      const response = await this.dishService.updateOne(params.id, updateData)
+
+      return { error: "", data: response }
+    }
+
+    @Delete(":id")
+    async deleteOne(@Param() params: any) {
+      await this.dishService.deleteOne(params.id)
     }
   }
--- src/modules/dish/dish.service.ts
+++ src/modules/dish/dish.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { DishDTO } from 'src/dto/dish.dto';
+import { Dish } from 'src/mongo/models/dish.model';
+import { DataType } from 'src/mongo/repositories/base.repository';
 import { DishRepository } from 'src/mongo/repositories/dish.repository';
 
 @Injectable()
@@ -6,4 +9,74 @@ export class DishService {
   constructor(
     private readonly dishRepository: DishRepository
   ) {}
+
+  async createOne(dishData: DishDTO): Promise<Dish> {
+    try {
+      const response = await this.dishRepository.insert(
+        {
+          name: dishData.name,
+          ingredients: dishData.ingredients,
+          price: dishData.price,
+          image: dishData.image,
+          description: dishData.description,
+          category: dishData.category,
+          timeCook: dishData.timeCook,
+          isAvailable: dishData.isAvailable,
+        }
+      )
+
+      return response as Dish
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException();
+    }
+  }
+
+  async findAll(): Promise<Dish[]> {
+    try {
+      const response = await this.dishRepository.findAll()
+
+      return response as Dish[]
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException();
+    }
+  }
+
+  async findOne(id: string): Promise<Dish> {
+    try {
+      const response = await this.dishRepository.findOneBy({ _id: id })
+
+      return response as Dish
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException();
+    }
+  }
+
+  async updateOne(id: string, dishData: DataType): Promise<Dish> {
+    try {
+      const isUpdate = await this.dishRepository.updateOneBy({ _id: id }, dishData)
+
+      if (!isUpdate) {
+        throw new BadRequestException();
+      }
+
+      const response = await this.findOne(id)
+
+      return response as Dish
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException();
+    }
+  }
+
+  async deleteOne(id: string) {
+    try {
+      await this.dishRepository.deleteOnyBy({ _id: id })
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException();
+    }
+  }
 }
