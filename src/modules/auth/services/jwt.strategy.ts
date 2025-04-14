63d78be4f555cc78d5b9a6ef58d5c052d533a455
Pierre <Pierre_gourgouillon@hotmail.com>
2025-04-14 23:27:26 +0200

    finish

--- src/modules/auth/services/jwt.strategy.ts
+++ src/modules/auth/services/jwt.strategy.ts
@@ -1,19 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import config from 'src/configs/config';
+import { AuthService } from 'src/modules/auth/services/auth.service';
+import { ConfigService } from '@nestjs/config';
+import { User } from 'src/modules/user/models/user.model';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+      private readonly authService: AuthService,
+      private readonly configService: ConfigService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get<string>('jwtSecret'),
       ignoreExpiration: false,
-      secretOrKey: config().jwtSecret || 'defaultSecret',
     });
   }
 
-  async validate(payload: any) {
-    return { userId: payload.sub };
+  async validate(payload: { sub: string }): Promise<User> {
+    const user = await this.authService.findOneById(payload.sub);
+
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    return user;
   }
 }
